f7335f5bc80e8b5c82b9248d667a41781107ea10
Alex Dunn <alexander.dunn@outlook.com>
2021-03-07 19:47:10 -0500

    setup for camera page and luis

---
+++ src/SuaveKeys/SuaveKeys.Clients/SuaveKeys.Clients/Services/LuisLanguageUnderstandingService.cs
@@ -0,0 +1,17 @@
+﻿using ServiceResult;
+using SuaveKeys.Clients.Models.Language;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuaveKeys.Clients.Services
+{
+    public class LuisLanguageUnderstandingService : ILanguageService
+    {
+        public Task<Result<Intent>> ProcessLanguage(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
