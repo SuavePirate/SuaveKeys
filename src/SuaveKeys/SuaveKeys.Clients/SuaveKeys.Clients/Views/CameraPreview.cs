2020c20ad5f15ce7c361615305f61b798caeb5bc
Alex Dunn <alexander.dunn@outlook.com>
2021-03-09 02:01:09 -0500

    work on camera support and implemented in uwp

---
+++ src/SuaveKeys/SuaveKeys.Clients/SuaveKeys.Clients/Views/CameraPreview.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.CommunityToolkit.UI.Views;
+using Xamarin.Forms;
+
+namespace SuaveKeys.Clients.Views
+{
+    /// <summary>
+    /// This is only for Windows (maybe mac?), so don't use renderers in the other platforms
+    /// </summary>
+    public class CameraPreview : View
+    {
+        public static readonly BindableProperty CameraProperty = BindableProperty.Create(
+            propertyName: "Camera",
+            returnType: typeof(CameraOptions),
+            declaringType: typeof(CameraPreview),
+            defaultValue: CameraOptions.Default);
+
+        public CameraOptions Camera
+        {
+            get { return (CameraOptions)GetValue(CameraProperty); }
+            set { SetValue(CameraProperty, value); }
+        }
+    }
+}
